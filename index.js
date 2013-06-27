cd207e4740111ae0b508e544b2416f34e54c91f7
Max Ogden <max@maxogden.com>
2013-06-26 23:01:29 -0700

    2.0.0 - terminal notifications, switch to readline

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var dgram = require('dgram')
 var fs = require('fs')
 var ansi = require('ansi')
 var spawn = require('child_process').spawn
+var readline = require('readline')
 var url = require('url')
 
 var socket = dgram.createSocket('udp4')
 var port = 12346
-var cursor = ansi(process.stdout)
 
 module.exports = function(opts) {
   if (!opts.them) opts.them = 'them'
@@ -17,28 +17,30 @@ module.exports = function(opts) {
   var target = url.parse(opts.target)
   var me = url.parse(opts.local)
   
-  process.stdin.setRawMode(true)
-  process.stdin.setEncoding('utf8')
-
+  var rl = readline.createInterface(process.stdin, process.stdout)
+  
+  rl.clearLine = function() {
+    rl.write(null, {ctrl: true, name: 'u'})
+  }
+  
+  var cursor = ansi(rl.output)
+  
+  rl.setPrompt('', 0)
+  rl.prompt()
+  
   // sending message
-  var message = ''
-  process.stdin.on('data', function(c) {
-    var str = c.toString()
-    c = str.charCodeAt(0)
-    if (c === 3) return process.exit()
-    if (c === 13) {
-      if (message === '') return
-      var buf = new Buffer(message)
-      socket.send(buf, 0, buf.length, target.port, target.hostname, function(err, bytes) {
-        if (err && err.code === 'ENOTFOUND') return console.log('could not deliver message :(')
-        else if (err) console.log('error!', err, err.message)
-        cursor.white().bg.black().write(' ' + opts.me + ' ')
-        cursor.reset().bg.reset().write(' ' + buf.toString() + '\n')
-      })
-      message = ''
-      return
-    }
-    message += str
+  rl.on('line', function(line) {
+    rl.clearLine()
+    rl.prompt()
+    var message = line.trim()
+    if (message === '') return
+    var buf = new Buffer(message)
+    socket.send(buf, 0, buf.length, target.port, target.hostname, function(err, bytes) {
+      if (err && err.code === 'ENOTFOUND') return console.log('could not deliver message :(')
+      else if (err) console.log('error!', err, err.message)
+      cursor.white().bg.black().write(' ' + opts.me + ' ')
+      cursor.reset().bg.reset().write(' ' + buf.toString() + '\n')
+    })
   })
 
 
@@ -46,7 +48,7 @@ module.exports = function(opts) {
   socket.on("message", function (msg, rinfo) {
     cursor.black().bg.cyan().write(' ' + opts.them + ' ')
     cursor.blue().bg.reset().write(' ' + msg.toString())
-    cursor.write('\n')
+    cursor.write('\u0007\n')
     cursor.reset()
     spawn("/usr/bin/afplay", ["/System/Library/Sounds/Submarine.aiff"])
   })
@@ -65,4 +67,3 @@ module.exports = function(opts) {
     socket.setMulticastTTL(16)
   })
 }
-
